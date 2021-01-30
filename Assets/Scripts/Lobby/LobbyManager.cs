ea3733a22af39aed0d1467cb55c1fc0c872d048f
kieran ryder <kieranlryder@gmail.com>
2021-01-30 19:00:12 +0000

    Re-enabled player number update

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -34,6 +35,11 @@ public class LobbyManager : NetworkBehaviour
             startButton.GetComponentInChildren<Text>().text = "Waiting for host";
             startButton.enabled = false;
         }
+    }
+
+    private void Update()
+    {
+        PlayerCountInt = NetworkServer.connections.Count;
     }
 
     public string GetLocalIPAddress()
