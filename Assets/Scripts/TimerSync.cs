98bcc3bcd51d2b8c5baf76831dd0ec99b4e11fe8
JamieOutram <jamie.outram@btinternet.com>
2021-01-31 14:48:53 +0000

    add timer prefab (in progress)

---
+++ Assets/Scripts/TimerSync.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class TimerSync : NetworkBehaviour
+{
+    public NetworkText text;
+
+    private void Update()
+    {
+        if (isServer)
+        {
+            text.SetText(ScoreManager.singleton.GetTimeRemaining());
+        }
+    }
+
+}
