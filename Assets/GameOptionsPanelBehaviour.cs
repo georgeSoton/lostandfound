42f2c13293bee51fafc703a9c188c429ae5c1dc6
JamieOutram <jamie.outram@btinternet.com>
2021-02-04 22:14:44 +0000

    Fix GameOptionsPanel client height issue

--- Assets/GameOptionsPanelBehaviour.cs
+++ Assets/GameOptionsPanelBehaviour.cs
@@ -4,39 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
 using Mirror;
-public class GameOptionsPanelBehaviour : NetworkBehaviour
+
+public class GameOptionsPanelBehaviour : MonoBehaviour
 {
-    [SerializeField] List<Toggle> levelSelectToggleList;
-    //public Dictionary<string, bool> levelSelectMap;
+    [SerializeField] public List<Toggle> levelSelectToggleList;
 
-    public override void OnStartClient()
-    {
-        base.OnStartClient();
-    }
-
-    public override void OnStartServer()
-    {
-        base.OnStartServer();
-    }
-
-    void Start()
-    {
-        if (isServer)
-        {
-            SyncToggleState();
-        }
-        if (isClientOnly)
-        {
-            SettingsManager.singleton.levelSelectMap.Callback += SetToggle;
-            foreach (Toggle t in levelSelectToggleList)
-            {
-                t.interactable = false;
-            }
-        }
-    }
-    // Start is called before the first frame update
     [Server]
-    void SyncToggleState()
+    public void SyncToggleState()
     {
         
         if(SceneChanger.singleton != null) {
@@ -72,7 +46,7 @@ public class GameOptionsPanelBehaviour : NetworkBehaviour
     }
 
     [Client]
-    void SetToggle(SyncDictionary<string, bool>.Operation op, string name, bool isOn)
+    internal void SetToggle(SyncDictionary<string, bool>.Operation op, string name, bool isOn)
     {
         Toggle t = levelSelectToggleList.SingleOrDefault(x => x.name == name);
         if (t != null)
---
+++ Assets/SyncSettings.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Mirror;
+public class SyncSettings : NetworkBehaviour
+{
+    //This Script is needed to prevent the panel object requiring a network Identity
+    [SerializeField]
+    public GameOptionsPanelBehaviour optionsPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isServer)
+        {
+            optionsPanel.SyncToggleState();
+        }
+        if (isClientOnly)
+        {
+            SettingsManager.singleton.levelSelectMap.Callback += optionsPanel.SetToggle;
+            foreach (Toggle t in optionsPanel.levelSelectToggleList)
+            {
+                t.interactable = false;
+            }
+        }
+    }
+}
