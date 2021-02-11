b625fd7a058dd8bd1e29cd28c221145c91cfda25
JamieOutram <jamie.outram@btinternet.com>
2021-02-11 21:07:15 +0000

    Add rating behaviour

---
+++ Assets/Scripts/Summary/RatingBehaviour.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class RatingBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1, 3)]
+    private int rating=1;
+    [SerializeField]
+    GameObject[] starInners;
+    [SerializeField]
+    [Range(0, 3)]
+    float showDelay;
+
+    int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject star in starInners)
+        {
+            star.transform.localScale = Vector3.zero;
+        }
+        ShowStars(3);
+    }
+
+    public void ShowStars(int rating)
+    {
+        this.rating = rating;
+        if(rating > 0)
+        {
+            ShowStarRecursive();
+        }
+    }
+
+    void ShowStarRecursive()
+    {
+        starInners[index].transform.DOScale(1, 2f).SetEase(Ease.OutElastic).Play();
+        index++;
+        if (index < rating)
+        {
+            Invoke(nameof(ShowStarRecursive), showDelay);
+        }
+    }
+
+}
